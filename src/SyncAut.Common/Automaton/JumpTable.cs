898b04b0ab2d2f5e4c4ef0268b1216334f51159b
etipikin <etipikin@gmail.com>
2017-03-20 20:24:00 +0500

    add JumpTable

---
+++ src/SyncAut.Common/Automaton/JumpTable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SyncAut.Common.Automaton
+{
+	public class JumpTable
+	{
+		private readonly Dictionary<char, int> letterActions = new Dictionary<char, int>();
+
+		public void AddJump(char letter, int newStateIndex)
+		{
+			letterActions.Add(letter, newStateIndex);
+		}
+
+		public int Jump(char letter)
+		{
+			return letterActions[letter];
+		}
+	}
+}
---
+++ tests/SyncAut.Tests/Common/Automaton/JumpTable_Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SyncAut.Common.Automaton
+{
+	[TestFixture]
+	public class JumpTable_Test
+	{
+		private JumpTable jumpTable;
+
+		[SetUp]
+		public void SetUp()
+		{
+			jumpTable = new JumpTable();
+		}
+
+		[Test]
+		public void SetJumps_ReadJumps()
+		{
+			jumpTable.AddJump('a', 10);
+			jumpTable.AddJump('b', 20);
+
+			Assert.That(jumpTable.Jump('a'), Is.EqualTo(10));
+			Assert.That(jumpTable.Jump('b'), Is.EqualTo(20));
+		}
+
+		[Test]
+		public void JumpNotSet_Error()
+		{
+			Assert.Throws<KeyNotFoundException>(() => jumpTable.Jump('a'));
+		}
+
+		[Test]
+		public void AddJumpByLetterTwice_Error()
+		{
+			jumpTable.AddJump('a', 5);
+			Assert.Throws<ArgumentException>(() => jumpTable.AddJump('a', 10));
+		}
+	}
+}
